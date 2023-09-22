923350dbfba398ee87e8594b525b447b271dfbd3
Ali Norouzifar <norouzifar.ali@gmail.com>
2023-09-22 15:57:31 +0200

    first commit!

--- functions/my_functions.py
+++ functions/my_functions.py
@@ -31,7 +31,7 @@ def log_to_tables(log, parameters):
             # print(f'{col} can be a case attribute')
             case_attributes.add(col)
 
-    case_table = df[case_attributes.union([case_id_name])].groupby(by=[case_id_name]).first()
+    case_table = df[list(case_attributes.union([case_id_name]))].groupby(by=[case_id_name]).first()
     case_table['trace'] = df.groupby(by=[case_id_name])[acyivity_name].apply(lambda x: tuple(list(x.astype(str))))
     case_table['duration'] = df.groupby(by=[case_id_name])[timestamp_name].apply(lambda x: int((x.iloc[-1] - x.iloc[0]).total_seconds() / time_unit))
     case_table = case_table.reset_index()
@@ -39,7 +39,7 @@ def log_to_tables(log, parameters):
 
     non_attributes = {'row_num'}
     event_attributes = set(df.columns) - case_attributes - non_attributes
-    event_table = df[event_attributes]
+    event_table = df[list(event_attributes)]
     event_table[timestamp_name] = pd.to_datetime(event_table[timestamp_name], utc=True)
     if 'EventOrder' in event_table.columns:
         event_table = event_table.sort_values([timestamp_name, 'EventOrder'], ascending=[True, True])
